33ab4a97396d4e449cd43d71c55359abb2b0cdfa
topseySuave <gabrielsuave17@gmail.com>
2018-08-22 07:51:27 +0100

    add the slider with images

--- App.js
+++ App.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import Expo from 'expo';
 import { Platform, StatusBar, View } from 'react-native';
-import { createStackNavigator } from "react-navigation";
+// import { createStackNavigator } from "react-navigation";
 import MainSwiper from './components/MainSwiper';
 import SelectPhoto from './screens/SelectPhoto';
 
-const ScreenRouter = createStackNavigator({
-  Home: { screen: SelectPhoto },
-  MainSwiper: { screen: MainSwiper },
-},
-{
-  navigationOptions: {
-    headerStyle: {
-      marginTop: Platform.OS === 'ios' ? Expo.Constants.statusBarHeight : 0,
-      backgroundColor: 'yellow',
-    },
-    headerTintColor: '#000000',
-  }
-}
-);
+// const ScreenRouter = createStackNavigator({
+//   Home: { screen: SelectPhoto },
+//   MainSwiper: { screen: MainSwiper },
+// },
+// {
+//   navigationOptions: {
+//     headerStyle: {
+//       marginTop: Platform.OS === 'ios' ? Expo.Constants.statusBarHeight : 0,
+//       backgroundColor: 'yellow',
+//       elevation: 0,
+//       shadowOpacity: 0
+//     },
+//     headerTintColor: '#000000',
+//   }
+// }
+// );
 
 export default class App extends React.PureComponent {
   render() {
@@ -28,7 +30,7 @@ export default class App extends React.PureComponent {
           backgroundColor="yellow"
           barStyle="dark-content"
         />
-        <ScreenRouter />
+        <MainSwiper />
       </React.Fragment>
     );
   }
--- components/MainSwiper.js
+++ components/MainSwiper.js
@@ -1,56 +1,135 @@
 import React from 'react';
-import Expo from 'expo';
-import { StyleSheet, Text, View, Platform } from 'react-native';
+import { Font, AppLoading, Constants } from 'expo';
+import {
+  StyleSheet,
+  Text, View,
+  Platform,
+  StatusBar, Dimensions,
+  Image, Animated,
+  PanResponder
+} from 'react-native';
 import { Content, Container } from 'native-base';
 import Swiper from 'react-native-swiper';
+import Carousel from 'react-native-snap-carousel';
+
+const Users = [
+  { id: 1, uri: require('../assets/img/demo/demo1.jpg') },
+  { id: 2, uri: require('../assets/img/demo/demo2.jpg') },
+  { id: 3, uri: require('../assets/img/demo/demo3.jpg') },
+];
+
+const IS_ANDROID = Platform.OS === 'android';
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 export default class MainSwiper extends React.Component {
+  state = {
+    fontLoaded: false,
+    currentIndex: 0
+  }
 
-  static navigationOptions = {
-    title: 'Happy Snap-dating',
-    headerStyle: {
-      marginTop: Platform.OS === 'ios' ? Expo.Constants.statusBarHeight : 0,
-      backgroundColor: '#FFFFFF'
-    }
-  };
+  // position = new Animated.ValueXY()
+
+  // componentWillMount() {
+  //   this.PanResponder = PanResponder.create({
+
+  //     onStartShouldSetPanResponder: (event, gestureState) => true,
+  //     onPanResponderMove: (event, gestureState) => {
+  //       this.position.setValue({ x: gestureState.dx, y: gestureState.dy })
+  //     },
+  //     onPanResponderRelease: (event, gestureState) => {}
+
+  //   });
+  // }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'montserrat-bold': require('../assets/fonts/Montserrat/Montserrat-Bold.ttf'),
+      'Arial': require('../assets/fonts/Arial/Arial.ttf')
+    });
+
+    this.setState({ fontLoaded: true });
+  }
+
+  renderSwipeCard = () => {
+    return Users.map((item, index) => {
+      return (
+        <View
+          key={index}
+          style={{
+            height: SCREEN_HEIGHT,
+            width: SCREEN_WIDTH,
+          }}
+        >
+          <Image
+            style={{ flex: 1, height: null, width: null, resizeMode: 'cover' }}
+            source={item.uri}
+          />
+        </View>
+      );
+    }).reverse();
+  }
 
   render() {
-    return (
-      <Container>
-        <Content>
-
-          <Swiper style={styles.wrapper} showsButtons={true} loop={false}>
-            <View style={styles.slider}>
-              <Text style={styles.text}>Hello Swiper</Text>
-            </View>
-            <View style={styles.slider}>
-              <Text style={styles.text}>Beautiful</Text>
-            </View>
-            <View style={styles.slider}>
-              <Text style={styles.text}>And simple</Text>
-            </View>
+    if (this.state.fontLoaded){
+      return (
+        <React.Fragment>
+          <View style={styles.headerStyle}>
+            <Text style={styles.headerText}>header</Text>
+          </View>
+          <Swiper
+            showsPagination={true}
+            loadMinimalLoader={<AppLoading />}
+            dotStyle={{ height: 5 }}
+            activeDotStyle={{ width: 100, height: 5, backgroundColor: 'yellow' }}
+            loop={false}
+            width={Platform.OS === 'ios' ? '100%' : SCREEN_WIDTH}
+          >
+            {this.renderSwipeCard()}
           </Swiper>
-
-        </Content>
-      </Container>
-    );
+        </React.Fragment>
+      );
+    } else {
+      return <AppLoading />
+    }
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerStyle: {
+    flex: 1,
+    zIndex: 1000,
+    backgroundColor: 'rgba(0, 0, 0, .5)',
+    position: 'absolute',
+    width: '100%',
+    height: 64,
+    marginTop: Constants.statusBarHeight,
+    // alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    justifyContent: 'center'
+  },
+  headerText: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+  swiperStyle: {
+    width: Platform.OS === 'ios' ? '100%' : SCREEN_WIDTH
+  },
   wrapper: {
+    // flex: 1,
   },
   slider: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#FFF',
+    backgroundColor: '#000',
   },
   text: {
     color: '#333',
